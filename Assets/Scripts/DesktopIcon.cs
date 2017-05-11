8a75424d2e17bdc57dbd2de6f842ea09d62d4d9e
FireFlyForLife <maiko.s.99@gmail.com>
2017-05-11 02:01:01 +0200

    initial commit

---
+++ Assets/Scripts/DesktopIcon.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[Serializable]
+public class IconSettings
+{
+    public Texture Icon;
+    public String Text = "Program.exe";
+}
+
+public class DesktopIcon : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    [SerializeField] Color selectedColor;
+    [SerializeField] RawImage underTextImage;
+    [SerializeField] RawImage Icon;
+    [SerializeField] Text Text;
+
+    [SerializeField]
+    IconSettings icon;
+
+    private Vector2 dragOffset = Vector2.zero;
+
+	void Start ()
+	{
+	    Icon.texture = icon.Icon;
+	    Text.text = icon.Text;
+	}
+	
+	void Update () {
+		
+	}
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        Icon.gameObject.GetComponent<CanvasRenderer>().SetAlpha(0.8f);
+        Icon.gameObject.GetComponent<CanvasRenderer>().SetColor(selectedColor);
+
+        underTextImage.enabled = true;
+
+        dragOffset = (Vector2)transform.position - eventData.position;
+    }
+
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        transform.position = eventData.position + dragOffset;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        Icon.gameObject.GetComponent<CanvasRenderer>().SetAlpha(1f);
+        Icon.gameObject.GetComponent<CanvasRenderer>().SetColor(Color.white);
+
+        underTextImage.enabled = false;
+
+        dragOffset = Vector2.zero;
+    }
+}
---
+++ Assets/Scripts/Time.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+
+public class Time : MonoBehaviour {
+
+    public Text timeText;
+    public GameObject contextMenu;
+    public Vector3 contextMenuReset;
+    public bool contextMenuOpen = false;
+    public string time;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        time = System.DateTime.Now.ToString("hh:mm");
+        timeText.text = time.ToString();
+
+        if(Input.GetMouseButtonDown(1 ))
+        {
+            Vector3 clickedPosition = Input.mousePosition;
+            Debug.Log(clickedPosition);
+            contextMenuOpen = true;
+            contextMenu.transform.position = clickedPosition;
+        }
+        if (contextMenuOpen == true && Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickedPosition = contextMenuReset;
+            contextMenuOpen = false;
+            contextMenu.transform.position = clickedPosition;
+        }
+    }
+}
