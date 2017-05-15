30a14195412df8882a8307d27f0ea46209c726d0
FireFlyForLife <maiko.s.99@gmail.com>
2017-05-15 03:35:50 +0200

    Added the UI framework for a Window.
    With buttons and menuitems and stuff
    
    The window is now dragable and the UI should be resizable.
    However no buttons do something at the moment

---
+++ Assets/Scripts/ButtonChildrenTranslator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonChildrenTranslator : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    public Vector2 Translation = new Vector2(1, -1);
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            Transform[] transforms = child.GetComponentsInChildren<Transform>();
+            foreach (Transform tr in transforms)
+            {
+                tr.position += (Vector3)Translation;
+            }
+        }
+    }
+
+    void IPointerUpHandler.OnPointerUp(PointerEventData eventData)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            var child = transform.GetChild(i);
+            Transform[] transforms = child.GetComponentsInChildren<Transform>();
+            foreach (Transform tr in transforms)
+            {
+                tr.position += (Vector3)(-Translation);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WindowHeader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WindowHeader : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+	Vector2 offset = Vector2.zero;
+    private Transform parent;
+
+    void Start()
+    {
+        parent = transform.parent;
+    }
+
+    void IBeginDragHandler.OnBeginDrag(PointerEventData eventData)
+    {
+        offset = (Vector2)parent.transform.position - eventData.position;
+    }
+
+    //TODO: Make this drag a box around and not directly move the window
+    void IDragHandler.OnDrag(PointerEventData eventData)
+    {
+        parent.transform.position = eventData.position + offset;
+    }
+
+    void IEndDragHandler.OnEndDrag(PointerEventData eventData)
+    {
+        offset = Vector2.zero;
+    }
+}
