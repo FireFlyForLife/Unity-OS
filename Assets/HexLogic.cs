1ccc74b3e759f27d6670ce77badaca6dfbdedb26
Brent Minkes <brentminkes@hotmail.com>
2017-06-08 22:47:02 +0200

    hexagon progress, json parser

--- Assets/HexLogic.cs
+++ Assets/HexLogic.cs
@@ -6,15 +6,18 @@ using UnityEngine.EventSystems;
 public class HexLogic : MonoBehaviour, ISelectHandler, IPointerClickHandler {
 
     public bool[] sides = new bool[6];
+    public int x;
+    public int y;
+    public bool active;
 	// Use this for initialization
 	void Start () {
 		
 	}
     void IPointerClickHandler.OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("clicked");
+        
         gameObject.transform.Rotate(0, 0, 60, Space.World);
-        ShiftRight(sides);
+        sides = ShiftRight(sides);
     }
     void ISelectHandler.OnSelect(BaseEventData eventData)
     {
@@ -24,19 +27,30 @@ public class HexLogic : MonoBehaviour, ISelectHandler, IPointerClickHandler {
     // Update is called once per frame
     void Update () {
     }
-
-    private bool[] ShiftRight(bool[] sides)
+    void CheckSides()
     {
-        bool[] shiftarr = new bool[sides.Length];
-
-        for (int i = 1; i < sides.Length; i++)
+        if (GameObject.Find("Hex_" + x + "_" + (y + 2)).transform.GetComponent<HexLogic>().active)
         {
-            shiftarr[i] = sides[i - 1];
+            GameObject hex_up = GameObject.Find("Hex_" + x + "_" + (y + 2));
+            GameObject hex_down = GameObject.Find("Hex_" + x + "_" + (y - 2));
+            GameObject hex_left_up = GameObject.Find("Hex_" + x + "_" + (y + 1));
+            GameObject hex_left_down = GameObject.Find("Hex_" + x + "_" + (y - 1));
+            GameObject hex_right_up = GameObject.Find("Hex_" + (x + 1) + "_" + (y + 1));
+            GameObject hex_right_down = GameObject.Find("Hex_" + (x + 1) + "_" + (y - 1));
+
         }
-
-        shiftarr[0] = sides[shiftarr.Length - 1];
-
-        return shiftarr;
     }
+        private bool[] ShiftRight(bool[] sides)
+        {
+            bool[] shiftarr = new bool[sides.Length];
 
+            for (int i = 1; i < sides.Length; i++)
+            {
+                shiftarr[i] = sides[i - 1];
+            }
+
+            shiftarr[0] = sides[shiftarr.Length - 1];
+
+            return shiftarr;
+        }
 }
--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -13,10 +13,16 @@ public class HexGrid : MonoBehaviour
     int height = 5;
     float offsetY = -43f;
     public Vector2 PositionOffset = new Vector2(120f, 0f);
+    public string json;
     public List<GameObject> hexagons;
+    public GameObject hexagonInfo;
+    public Texture2D[] lines;
+    private JSONObject jsonObject;
 
     void Start()
     {
+        
+        Debug.Log(Environment.Version);
         for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
@@ -36,6 +42,8 @@ public class HexGrid : MonoBehaviour
                 }
                 hex.GetComponent<RectTransform>().anchoredPosition = new Vector2(hw * x * 1.5f + offsetX + PositionOffset.x, hy * y * 0.5f + offsetY + PositionOffset.y);
                 hex.name = "Hex_" + x + "_" + y;
+                hex.transform.GetComponent<HexLogic>().x = x;
+                hex.transform.GetComponent<HexLogic>().y = y;
                 hexagons.Add(hex.transform.GetChild(0).gameObject);
                 if (x == 2)
                 {
@@ -48,7 +56,29 @@ public class HexGrid : MonoBehaviour
                 //Debug.Log("Hex_" + x + "_" + y + "=" + (hw + offsetX) * (x + 1) +" "+ (hy + offsetY) * (y + 1));
             }
         }
+        populateFromJson();
+    }
+    private void populateFromJson()
+    {
+        jsonObject = new JSONObject(JSONObject.Type.ARRAY);
+        jsonObject = new JSONObject(json);
 
+        foreach (JSONObject j in jsonObject.list)
+        {
+            long id = j.GetField("id").i;
+            long x = j.GetField("x").i;
+            long y = j.GetField("y").i;
+            long rotation = j.GetField("rotation").i;
+
+            switch (id)
+            {
+                case 0:
+                    //lines[0];
+                    break;
+            }
+
+            Debug.Log(id + " " + x + " " + y + " " + rotation);
+        }
     }
     void Update()
     {
