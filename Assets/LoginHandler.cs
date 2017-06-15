ad6c53e28388545b3577e5e60e2a6fa840e6210f
Brent Minkes <brentminkes@hotmail.com>
2017-06-15 23:56:09 +0200

    working login screen

--- Assets/LoginHandler.cs
+++ Assets/LoginHandler.cs
@@ -1,16 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class LoginHandler : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
+    public GameObject password;
+    public GameObject LoadingText;
+    public GameObject LoginScreen;
+    private InputField passwordtext;
+    // Use this for initialization
+    void Start () {
+        passwordtext = password.GetComponent<InputField>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        if (Input.GetKey(KeyCode.Return)){
+            if (passwordtext.text == "kavep")
+            {
+                LoginScreen.SetActive(true);
+                LoadingText.SetActive(true);
+                StartCoroutine(LoginScreenWait());
+                
+            } 
+        }
 	}
+    IEnumerator LoginScreenWait()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("OS1", LoadSceneMode.Additive);
+    }
 }
