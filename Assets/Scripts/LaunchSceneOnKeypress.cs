14b81227674066f56b38292987b72c9c37324349
FireFlyForLife <maiko.s.99@gmail.com>
2017-06-21 16:47:55 +0200

    Added a cheat to skip the login page

---
+++ Assets/Scripts/LaunchSceneOnKeypress.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LaunchSceneOnKeypress : MonoBehaviour
+{
+    public KeyCode[] Keys;
+    public int SceneToSwitch;
+	
+	void Update () {
+	    if (AreAllKeysPressed())
+	    {
+	        SceneManager.LoadScene(SceneToSwitch);
+	    }
+	}
+
+    bool AreAllKeysPressed()
+    {
+        foreach (KeyCode code in Keys)
+        {
+            if (!Input.GetKey(code))
+                return false;
+        }
+
+        return true;
+    }
+}
